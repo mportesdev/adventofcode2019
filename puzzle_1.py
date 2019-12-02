08c538dc9530b642a0054cab4a177e7dd5999561
myrmica-habilis <michalportes1@gmail.com>
2019-12-02 11:04:31 +0100

    Solution #1

---
+++ puzzle_1.py
@@ -0,0 +1,24 @@
+# Part 1
+
+
+def fuel_from_mass(mass):
+    return mass // 3 - 2
+
+
+with open('puzzle_1_input') as f:
+    print(f'Solution: {sum(fuel_from_mass(int(line)) for line in f)}')
+
+
+# Part 2
+
+
+def fuel_from_mass_2(mass):
+    result = 0
+    while (fuel := mass // 3 - 2) > 0:
+        result += fuel
+        mass = fuel
+    return result
+
+
+with open('puzzle_1_input') as f:
+    print(f'Solution: {sum(fuel_from_mass_2(int(line)) for line in f)}')

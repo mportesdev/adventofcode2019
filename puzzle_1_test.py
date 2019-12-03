ee431f428ee1c8b5b340f5654f2406a4b1429982
myrmica-habilis <michalportes1@gmail.com>
2019-12-03 14:40:49 +0100

    Add test assertions

--- puzzle_1_test.py
+++ puzzle_1_test.py
@@ -5,5 +5,10 @@ def test_fuel_from_mass():
     assert fuel_from_mass(0) == 0
     assert fuel_from_mass(1) == 0
     assert fuel_from_mass(5) == 0
+    assert fuel_from_mass(8) == 0
+    assert fuel_from_mass(9) == 1
+    assert fuel_from_mass(11) == 1
+    assert fuel_from_mass(12) == 2
     assert fuel_from_mass(100) == 31
     assert fuel_from_mass(130129) == 43374
+    assert fuel_from_mass(1_000_000) == 333331

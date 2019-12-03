0e702b2adabb420408a35bfea5d7f9d78cc315f1
myrmica-habilis <michalportes1@gmail.com>
2019-12-03 14:15:24 +0100

    Add test assertions

--- puzzle_3_test.py
+++ puzzle_3_test.py
@@ -3,12 +3,19 @@ from puzzle_3 import inclusive_range
 
 def test_inclusive_range():
     assert inclusive_range(0) == range(1)
+    assert list(inclusive_range(0)) == [0]
 
     assert inclusive_range(1) == range(2)
+    assert list(inclusive_range(1)) == [0, 1]
+
     assert inclusive_range(-1) == range(0, -2, -1)
+    assert list(inclusive_range(-1)) == [0, -1]
 
-    assert inclusive_range(1000) == range(1001)
-    assert inclusive_range(-1000) == range(0, -1001, -1)
+    assert 1000 in inclusive_range(1000)
+    assert -1000 in inclusive_range(-1000)
 
-    assert list(inclusive_range(4)) == [0, 1, 2, 3, 4]
-    assert list(inclusive_range(-4)) == [0, -1, -2, -3, -4]
+    assert inclusive_range(1_000_000) == range(1_000_001)
+    assert inclusive_range(-1_000_000) == range(0, -1_000_001, -1)
+
+    assert list(inclusive_range(8)) == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    assert list(inclusive_range(-8)) == [0, -1, -2, -3, -4, -5, -6, -7, -8]

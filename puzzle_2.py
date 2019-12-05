f4a36cbd2a23786fb24233dd246d6937cb351bd7
myrmica-habilis <michalportes1@gmail.com>
2019-12-05 10:59:38 +0100

    Do not return list that was modified in-place

--- puzzle_2.py
+++ puzzle_2.py
@@ -2,13 +2,14 @@
 
 
 def execute_program(memory):
+    """Run the Intcode program, modifying the memory in-place."""
     # instruction pointer
     ip = 0
 
     while True:
         opcode = memory[ip]
         if opcode == 99:
-            return memory
+            return
         elif opcode in (1, 2):
             addr_1, addr_2, target_addr = memory[ip + 1:ip + 4]
             memory[target_addr] = (memory[addr_1] + memory[addr_2]
@@ -21,7 +22,8 @@ with open('puzzle_2_input') as f:
     memory = [int(elem) for elem in f.read().split(',')]
 
 memory[1:3] = [12, 2]
-print(f'Solution: {execute_program(memory)[0]}')
+execute_program(memory)
+print(f'Solution: {memory[0]}')
 
 
 # Part 2
@@ -35,6 +37,7 @@ with open('puzzle_2_input') as f:
 for noun, verb in product(range(100), range(100)):
     memory = initial_state[:]
     memory[1:3] = [noun, verb]
-    if execute_program(memory)[0] == 19690720:
+    execute_program(memory)
+    if memory[0] == 19690720:
         print(f'Solution: {100*noun + verb}')
         break
